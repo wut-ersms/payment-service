af8cc4ae43534d4e7f4ab6d15e089169ddea8604
Daniel Tomala <danieltomala01@gmail.com>
2025-05-27 23:01:49 +0200

    Add envs

--- src/main/java/com/wut/ersms/paymentservice/MongoConfiguration.java
+++ src/main/java/com/wut/ersms/paymentservice/MongoConfiguration.java
@@ -5,8 +5,10 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
@@ -28,5 +30,10 @@ public class MongoConfiguration extends AbstractMongoClientConfiguration {
                 .applyConnectionString(connectionString)
                 .build();
         return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), getDatabaseName());
     }
 }
